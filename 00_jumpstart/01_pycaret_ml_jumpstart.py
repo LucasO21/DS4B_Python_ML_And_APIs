847bde8ac097d0966e193fb84897a62e5c087ca2
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-05-26 15:24:49 -0400

    jumpstart part 1 complete

--- 00_jumpstart/01_pycaret_ml_jumpstart.py
+++ 00_jumpstart/01_pycaret_ml_jumpstart.py
@@ -16,41 +16,87 @@ import pycaret.classification as clf
 # 1.0 READ DATA ----
 
 # Connect to SQL Database ----
+engine = sql.create_engine("sqlite:///00_database/crm_database.sqlite")
 
+conn = engine.connect()
+
+sql.inspect(engine).get_table_names()
 
 # * Subscribers ---
-
+subscribers_df = pd.read_sql(
+    sql="select * from Subscribers",
+    con=conn
+)
 
 # * Transactions
+transactions_df = pd.read_sql(
+    sql="select * from Transactions",
+    con=conn
+)
 
 
 # *Close Connection ----
+conn.close()
 
 
 # 2.0 SIMPLIFIED DATA PREP ----
+subscribers_joined_df = subscribers_df.copy()
+
+emails_made_purchase = transactions_df["user_email"].unique()
+
+subscribers_joined_df["made_purchase"] = subscribers_joined_df["user_email"] \
+    .isin(emails_made_purchase) \
+    .astype("int")
 
 
 # 3.0 QUICKSTART MACHINE LEARNING WITH PYCARET ----
 
 # * Subset the data ----
+df = subscribers_joined_df[["member_rating", "country_code", "made_purchase"]]
 
 
 # * Setup the Classifier ----
+clf_1 = clf.setup(
+    data=df,
+    target="made_purchase",
+    train_size=0.8,
+    session_id=123
+)
 
 
 # * Make A Machine Learning Model ----
+xgb_model = clf.create_model(estimator="xgboost")
 
 
 # * Finalize the model ----
+xgb_model_finalized = clf.finalize_model(xgb_model)
 
 
 # * Predict -----
+new_df = pd.DataFrame(
+    dict(member_rating=[4], country_code=["us"])
+)
+
+clf.predict_model(
+    estimator=xgb_model_finalized,
+    data=new_df,
+    raw_score=True
+)
 
 
 # * Save the Model ----
+os.mkdir("00_jumpstart/models")
+
+clf.save_model(
+    model=xgb_model_finalized,
+    model_name="00_jumpstart/models/xgb_model_finalized"
+)
 
 
 # * Load the model -----
+clf.load_model(
+    model_name="00_jumpstart/models/xgb_model_finalized"
+)
 
 
 # CONCLUSIONS:
@@ -65,6 +111,3 @@ import pycaret.classification as clf
 # * WHAT ARE THE KEY FEATURES IN THE MODEL?
 # * CAN WE EXPLAIN WHY CUSTOMERS ARE BUYING / NOT BUYING?
 # * CAN THE COMPANY MAKE A RETURN ON INVESTMENT FROM THIS MODEL?
-
-
-# %%
