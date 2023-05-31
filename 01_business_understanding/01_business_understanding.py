6d2a28f632998f0cb4c21fb16caf729f73981741
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-05-31 08:00:04 -0400

    section 1.5 complete

--- 01_business_understanding/01_business_understanding.py
+++ 01_business_understanding/01_business_understanding.py
@@ -7,7 +7,11 @@
 import pandas as pd
 import numpy as np
 import janitor as jn
+from itertools import product
 import plotly.express as px
+from mizani.formatters import custom_format
+from plotnine import *
+import locale
 
 
 # BUSINESS SCIENCE PROBLEM FRAMEWORK ----
@@ -151,7 +155,7 @@ def cost_calc_monthly_cost_table(
     return cost_table_df
 
 
-df = cost_calc_monthly_cost_table(
+cost_calc_monthly_cost_table(
     email_list_size=50000,
     sales_emails_per_month=1,
     unsub_rate_per_sales_email=0.001,
@@ -170,28 +174,291 @@ def cost_total_unsub_cost(cost_table):
     return summary_df
 
 
-cost_total_unsub_cost(df)
+cost_total_unsub_cost(cost_table_df)
 
 # ARE OBJECTIVES BEING MET?
 # - We can see a large cost due to unsubscription
 # - However, some attributes may vary causing costs to change
 
 
-# SYNTHESIZE OUTCOMES (COST SIMULATION) ----
+# SYNTHESIZE OUTCOMES (COST SIMULATION) ----------------------------------------------
 # - Make a cartesian product of inputs that can vary
 # - Use list comprehension to perform simulation
 # - Visualize results
 
 # Cartesian Product (Expand Grid)
 
+data_dict = dict(
+    email_list_monthly_growth_rate=np.linspace(0, 0.05, num=10),
+    customer_conversion_rate=np.linspace(0.04, 0.06, num=3)
+)
 
-# Function
+combinations = list(
+    product(
+        data_dict['email_list_monthly_growth_rate'],
+        data_dict['customer_conversion_rate']
+    )
+)
+
+parameter_grid_df = pd.DataFrame(
+    combinations,
+    columns=['email_list_monthly_growth_rate', 'customer_conversion_rate']
+)
+
+# parameter_grid_df = jn.expand_grid(others=data_dict)
 
 
-# VISUALIZE COSTS
+# List Comprehension
+def temporary_function(x, y):
+
+    cost_table_df = cost_calc_monthly_cost_table(
+        email_list_growth_rate=x,
+        customer_conversion_rate=y
+    )
+
+    summary_df = cost_total_unsub_cost(cost_table_df)
+
+    return summary_df
 
 
-# Function: Plot Simulated Unsubscriber Costs
+temporary_function(x=0.035, y=0.05)
+
+
+summary_list = [
+    temporary_function(x, y) for x, y in zip(
+        parameter_grid_df["email_list_monthly_growth_rate"],
+        parameter_grid_df["customer_conversion_rate"]
+    )
+]
+
+simulation_results_df = pd.concat(summary_list, axis=0) \
+    .reset_index() \
+    .drop("index", axis=1) \
+    .merge(parameter_grid_df, left_index=True, right_index=True)
+
+
+# Function -------------------------------------------------------------------------------------
+
+
+def cost_simulate_unsub_cost(
+    email_list_monthly_growth_rate=[0, 0.035],
+    customer_conversion_rate=[0.04, 0.05, 0.06],
+    **kwargs
+):
+
+    # -- Parameter Grid -- #
+    data_dict = dict(
+        email_list_monthly_growth_rate=email_list_monthly_growth_rate,
+        customer_conversion_rate=customer_conversion_rate
+    )
+
+    combinations = list(
+        product(
+            data_dict['email_list_monthly_growth_rate'],
+            data_dict['customer_conversion_rate']
+        )
+    )
+
+    parameter_grid_df = pd.DataFrame(
+        combinations,
+        columns=['email_list_monthly_growth_rate', 'customer_conversion_rate']
+    )
+
+    # -- Temp Function -- #
+    def temporary_function(x, y):
+
+        cost_table_df = cost_calc_monthly_cost_table(
+            email_list_growth_rate=x,
+            customer_conversion_rate=y,
+            **kwargs
+        )
+
+        summary_df = cost_total_unsub_cost(cost_table_df)
+
+        return summary_df
+
+    # -- List Comprehension -- #
+    summary_list = [
+        temporary_function(x, y) for x, y in zip(
+            parameter_grid_df["email_list_monthly_growth_rate"],
+            parameter_grid_df["customer_conversion_rate"]
+        )
+    ]
+
+    simulation_results_df = pd.concat(summary_list, axis=0) \
+        .reset_index() \
+        .drop("index", axis=1) \
+        .merge(parameter_grid_df, left_index=True, right_index=True)
+
+    # -- Return -- #
+    return simulation_results_df
+
+
+cost_simulate_unsub_cost()
+
+
+# VISUALIZE COSTS ---------------------------------------------------------------------------
+
+# Plotly
+simulation_results_wide_df = cost_simulate_unsub_cost(
+    email_list_monthly_growth_rate=[0.01, 0.02],
+    customer_conversion_rate=[0.04, 0.06],
+    email_list_size=100000
+) \
+    .drop("cost_no_growth", axis=1) \
+    .pivot(
+        index="email_list_monthly_growth_rate",
+        columns="customer_conversion_rate",
+        values="cost_with_growth"
+)
+
+px.imshow(
+    simulation_results_wide_df,
+    origin="lower",
+    aspect="auto",
+    title="Lead Cost Simulation",
+    labels=dict(x="Customer Conversion Rate",
+                y="Monthly Email Growth Rate",
+                color="Unsubscrib Cost")
+)
+
+# Plotnine
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+df = cost_simulate_unsub_cost(
+    email_list_monthly_growth_rate=[0.01, 0.02],
+    customer_conversion_rate=[0.04, 0.06],
+    email_list_size=100000
+) \
+    .drop("cost_no_growth", axis=1) \
+    .assign(cost_with_growth_text=lambda x: x['cost_with_growth'].apply(lambda y: '${:.2f}M'.format(y / 1000000))) \
+    .assign(growth_rate_text=lambda x: x["email_list_monthly_growth_rate"].apply(lambda y: '{:.1%}'.format(y))) \
+    .assign(conv_rate_text=lambda x: x["customer_conversion_rate"].apply(lambda y: '{:.1%}'.format(y))) \
+    .assign(
+        label_text=lambda x: "Cost: " + x["cost_with_growth_text"].str.cat([
+            "Growth Rate: " + x['growth_rate_text'].astype(str).str.cat([
+                "Conv Rate: " + x["conv_rate_text"].astype(str)
+            ], sep="\n")
+        ], sep="\n")
+)
+
+(
+    ggplot(df, aes("email_list_monthly_growth_rate",
+           "customer_conversion_rate", fill="cost_with_growth"))
+    + geom_tile()
+    + geom_text(aes(label="label_text"), size=8)
+    + theme_bw()
+    + labs(
+        title="Lead Cost Simulation",
+        x="Email List Growth Rate (%)",
+        y="Customer Converstion Rate (%)",
+        fill="Cost with Growth"
+    )
+    + theme(
+        axis_text=element_text(size=8),
+        axis_title=element_text(size=8),
+        legend_title=element_text(size=9),
+        legend_text=element_text(size=7),
+        legend_key_size=10
+    )
+)
+
+# Function: Plot Simulated Unsubscriber Costs ---------------------------------------
+
+
+def cost_plot_simulated_unsub_costs(simulation_results):
+
+    simulation_results_wide_df = simulation_results \
+        .drop("cost_no_growth", axis=1) \
+        .pivot(
+            index="email_list_monthly_growth_rate",
+            columns="customer_conversion_rate",
+            values="cost_with_growth"
+        )
+
+    fig = px.imshow(
+        simulation_results_wide_df,
+        origin="lower",
+        aspect="auto",
+        text_auto=True,
+        title="Lead Cost Simulation",
+        labels=dict(
+            x="Customer Conversion Rate",
+            y="Monthly Email Growth Rate",
+            color="Unsubscrib Cost"
+        )
+    )
+
+    fig.update_layout(
+        xaxis=dict(title=dict(font=dict(size=11))),
+        yaxis=dict(title=dict(font=dict(size=11)))
+    )
+
+    return fig
+
+
+cost_simulate_unsub_cost(
+    email_list_monthly_growth_rate=[0.01, 0.02, 0.03],
+    customer_conversion_rate=[0.04, 0.05, 0.06],
+    email_list_size=100000
+) \
+    .pipe(cost_plot_simulated_unsub_costs)
+
+# Plotnine
+
+
+def cost_plot_simulated_unsub_costs_plotnine(simulation_results):
+
+    simulation_results_wide_df = simulation_results \
+        .drop("cost_no_growth", axis=1) \
+        .assign(cost_with_growth_text=lambda x: x['cost_with_growth'].apply(lambda y: '${:.2f}M'.format(y / 1000000))) \
+        .assign(growth_rate_text=lambda x: x["email_list_monthly_growth_rate"].apply(lambda y: '{:.1%}'.format(y))) \
+        .assign(conv_rate_text=lambda x: x["customer_conversion_rate"].apply(lambda y: '{:.1%}'.format(y))) \
+        .assign(
+            label_text=lambda x: "Cost: " + x["cost_with_growth_text"].str.cat([
+                "Growth Rate: " + x['growth_rate_text'].astype(str).str.cat([
+                    "Conv Rate: " + x["conv_rate_text"].astype(str)
+                ], sep="\n")
+            ], sep="\n")
+        )
+
+    p = (
+        ggplot(
+            simulation_results_wide_df,
+            aes(
+                "email_list_monthly_growth_rate",
+                "customer_conversion_rate",
+                fill="cost_with_growth"
+            )
+        )
+        + geom_tile()
+        + geom_text(aes(label="label_text"), size=8)
+        + theme_bw()
+        + labs(
+            title="Lead Cost Simulation",
+            x="Email List Growth Rate (%)",
+            y="Customer Converstion Rate (%)",
+            fill="Cost with Growth"
+        )
+        + theme(
+            axis_text=element_text(size=8),
+            axis_title=element_text(size=8),
+            legend_title=element_text(size=9),
+            legend_text=element_text(size=7),
+            legend_key_size=10,
+            legend_position="none"
+        )
+    )
+
+    return p
+
+
+cost_simulate_unsub_cost(
+    email_list_monthly_growth_rate=[0.01, 0.02, 0.03],
+    customer_conversion_rate=[0.04, 0.05, 0.06],
+    email_list_size=100000
+) \
+    .pipe(cost_plot_simulated_unsub_costs_plotnine)
 
 
 # ARE OBJECTIVES BEING MET?
