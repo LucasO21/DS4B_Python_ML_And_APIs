bbe55d5074710c8549cbccf86f41c877a2877d02
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-06-17 07:49:02 -0400

    scikit-learn code along; complete

--- 05_advanced_ml_sklearn_h2o/01_machine_learning_scikit_learn.py
+++ 05_advanced_ml_sklearn_h2o/01_machine_learning_scikit_learn.py
@@ -23,6 +23,7 @@ from sklearn.metrics import roc_auc_score, confusion_matrix
 
 from xgboost import XGBClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
 # Lead Scoring
 import email_lead_scoring as els
@@ -57,55 +58,119 @@ mod_1.__dict__["steps"]
 
 # 3.1 DATA PREPARATION
 
+# Get X and y
+X = leads_df\
+    .drop(
+        ["mailchimp_id", "user_full_name", "user_email", 
+         "email_provider", "made_purchase", "optin_time"], 
+        axis = 1
+    )
 
+y = leads_df["made_purchase"]
 
+# Train Test Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 123)
 
+# ========================================================================
 # 3.2 CREATING A SKLEARN PIPELINE ----
+# ========================================================================
 
 # Instantiate an Encoder & Connect to a Column
 
+ohe = OneHotEncoder(handle_unknown = "ignore")
+
+transformer = make_column_transformer( (ohe, ["country_code"]) )
 
 
 # Make a Pipeline
-
-
+pipeline_rf = make_pipeline( transformer, RandomForestClassifier() )
 
 
 # Fit & Predict with a Pipeline
+pipeline_rf.fit(X_train, y_train)
+
+pipeline_rf.predict(X_test)
+
+pipeline_rf.predict_proba(X_test)
 
 
-
-
+# ========================================================================
 # Metrics
+# ========================================================================
+
+# - Accuracy
+pipeline_rf.score(X_test, y_test)
+
+# - AUC ROC
+predicted_class_rf = pipeline_rf.predict_proba(X_test)[:, 1] > 0.035
+
+roc_auc_score(y_true = y_test, y_score = predicted_class_rf)
+
+confusion_matrix(y_true = y_test, y_pred = predicted_class_rf)
 
 
-
-
+# ========================================================================
 # 4.0 GRIDSEARCH -----
+# ========================================================================
 
-# Grid Search CV 
+# - Grid Search CV 
+grid_xgb = GridSearchCV(
+    estimator  = XGBClassifier(),
+    param_grid =  {"learning_rate": [0.1, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5]},
+    cv         = 5,
+    refit      = True,
+    scoring    = "roc_auc"
+)
 
 
+# - Make A Pipeline With GridSearch
+pipeline_xgb = make_pipeline(transformer, grid_xgb)
 
-# Make A Pipeline With GridSearch
+pipeline_xgb.fit(X_train, y_train)
+
+pipeline_xgb[1].best_params_
 
 
+# - Metrics
+predicted_class_xgb = pipeline_xgb.predict_proba(X_test)[:, 1] > 0.035
 
+roc_auc_score(y_true = y_test, y_score = predicted_class_xgb)
 
-# Metrics
-
+confusion_matrix(y_true = y_test, y_pred = predicted_class_xgb)
 
 
 
 # 5.0 PCYARET COMPARISON ----- 
+mod_1 = clf.load_model("models/blended_tuned_models_final")
+predicted_class_pycaret = mod_1.predict_proba(leads_df.iloc[X_test.index])[:,1] > 0.035
+
+roc_auc_score(y_true = y_test, y_score = predicted_class_pycaret)
 
 
+# ========================================================================
+# SAVE / LOAD ----
+# ========================================================================
+import joblib
+
+# - Save
+joblib.dump(pipeline_xgb, "models/scikit_learn/pipeline_xgb.pkl")
+
+# - Load
+joblib.load("models/scikit_learn/pipeline_xgb.pkl")
 
 
+# ========================================================================
 # CONCLUSIONS ----
-
+# ========================================================================
 # 1. See the benefit of using Pycaret (or AutoML)
 # 2. Faster to get results than Scikit Learn
 # 3. Handles a lot of the cumbersome preprocessing
 # 4. The result is a Scikit Learn Pipeline
+
+# ========================================================================
+# ========================================================================
+# ========================================================================
+# ========================================================================
+
+
 
