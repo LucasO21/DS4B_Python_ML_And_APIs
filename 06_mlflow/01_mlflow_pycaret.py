2b36be67c86f14fc09f42b0eea997bc21b214959
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-11-10 09:34:42 -0500

    delete old models, mlflow pycaret complete

--- 06_mlflow/01_mlflow_pycaret.py
+++ 06_mlflow/01_mlflow_pycaret.py
@@ -1,6 +1,6 @@
 # BUSINESS SCIENCE UNIVERSITY
 # COURSE: DS4B 201-P PYTHON MACHINE LEARNING
-# MODULE 6: MLFLOW 
+# MODULE 6: MLFLOW
 # PART 1: PYCARET INTEGRATION
 # ----
 
@@ -17,7 +17,7 @@ import mlflow
 # Lead Scoring
 import email_lead_scoring as els
 
-# RECAP 
+# RECAP
 
 leads_tags_df = els.db_read_and_process_els_data()
 
@@ -38,7 +38,7 @@ leads_tags_df = els.db_read_and_process_els_data()
 # http://localhost:5000/
 
 # 1. GUI OVERVIEW: HOW TO FIND THE MODELS YOU'RE LOOKING FOR
-# 2. SORTING: AUC 
+# 2. SORTING: AUC
 # 3. SEARCHING: tags.Source = 'finalize_model'
 # 4. DRILLING INTO MODELS
 
@@ -80,12 +80,14 @@ logs_df = mlflow.search_runs(experiment_ids=experiment_1.experiment_id)
 
 best_run_id = logs_df \
     .query("`tags.Source` in ['finalize_model']") \
-    .sort_values('metrics.AUC', ascending=False)['run_id'][0]
-
+    .sort_values('metrics.AUC', ascending=False) \
+    .head(1)['run_id'] \
+    .iloc[0]
 
 # pycaret interface to get experiments
 clf.get_logs(experiment_name = "email_lead_scoring_0")
 
+# Note - In production, we will only want to use models that have been finalized (source tag)
 
 
 # ========================================================================
@@ -98,16 +100,16 @@ best_run_id
 mlflow.list_run_infos("1")[0]
 
 # Finding Runs from Run ID
-best_run_details = mlflow.get_run("83a295ee41844a0a81bc832102eef2a1")
+best_run_details = mlflow.get_run("98eb5b2fdc374e938c709c52dff87a35")
 
 
 # ========================================================================
 # 5.0 MAKING PREDICTIONS ----
 # ========================================================================
 
-# Using the mlflow model 
+# Using the mlflow model
 import mlflow
-logged_model = 'runs:/83a295ee41844a0a81bc832102eef2a1/model'
+logged_model = 'runs:/98eb5b2fdc374e938c709c52dff87a35/model'
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.pyfunc.load_model(logged_model)
@@ -117,10 +119,9 @@ import pandas as pd
 loaded_model.predict(leads_tags_df)
 
 
-
 # Issue - Mlflow does not give probability. We need probabilities for lead scoring.
 
-# Solution 1 - Extract out the sklearn model and use the 
+# Solution 1 - Extract out the sklearn model and use the
 #   sklearn .predict_proba() method
 loaded_model._model_impl.predict_proba(leads_tags_df)[:,1]
 
@@ -140,5 +141,5 @@ clf.load_model(
 # CONCLUSIONS ----
 # 1. Pycaret simplifies the logging process for mlflow
 # 2. Pycaret includes a lot of detail (saved metrics) by default
-# 3. We haven't actually logged anything of our own 
+# 3. We haven't actually logged anything of our own
 # 4. Let's explore MLFlow deeper with H2O (We can actually use this process for Scikit Learn too!)
