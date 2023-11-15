43c722b84c19834554a9c66137f9b00c390a1ac2
Lucas Okwudishu <62886078+LucasO21@users.noreply.github.com>
2023-11-15 07:25:48 -0500

    wip: section 7.2; function 3 complete

--- 07_return_on_investment/02_threshold_optimization.py
+++ 07_return_on_investment/02_threshold_optimization.py
@@ -1,49 +1,231 @@
 # BUSINESS SCIENCE UNIVERSITY
 # COURSE: DS4B 201-P PYTHON MACHINE LEARNING
-# MODULE 7: ROI  
+# MODULE 7: ROI
 # PART 2: THRESHOLD OPTIMIZATION & PROFIT MAXIMIZATION
-# ----
+# ---------------------------------------------------------------------------- #
 
-# LIBRARIES -----
 
+# ---------------------------------------------------------------------------- #
+#                                   LIBRARIES                                  #
+# ---------------------------------------------------------------------------- #
 import pandas as pd
 import numpy as np
 import plotly.express as px
 import email_lead_scoring as els
 
-# RECAP ----
 
+# ---------------------------------------------------------------------------- #
+#                                  RECAP                                       #
+# ---------------------------------------------------------------------------- #
 leads_df = els.db_read_and_process_els_data()
 
-leads_scored_df = els.model_score_leads(leads_df)
+leads_scored_df = els.model_score_leads(
+	data       = leads_df,
+	model_path = "models/pycaret/xgb_model_single_tuned_finalized"
+)
 
 
-# 1.0 MAKE THE LEAD STRATEGY FROM THE SCORED SUBSCRIBERS:
+ # ---------------------------------------------------------------------------- #
+ #            1.0 MAKE THE LEAD STRATEGY FROM THE SCORED SUBSCRIBERS:           #
+ # ---------------------------------------------------------------------------- #
+
 #   lead_make_strategy()
 
+def lead_make_strategy(
+    data               = leads_scored_df,
+    thresh             = 0.95,
+    for_marketing_team = False,
+    verbose            = False
+):
+
+    # ranking leads
+    leads_ranked_df = (
+        data
+        .sort_values('Score', ascending=False)
+        .assign(rank=lambda x: np.arange(0, len(x['made_purchase'])) + 1)
+        .assign(gain=lambda x: np.cumsum(
+            x['made_purchase']) / np.sum(x['made_purchase'])
+        )
+    )
+
+    # make the strategy
+    strategy_df = (
+        leads_ranked_df
+        .assign(category=lambda x: np.where(x['gain'] <= thresh, 'Hot-Lead', 'Cold-Lead'))
+    )
+
+    if for_marketing_team:
+        strategy_for_marketing_df = (
+            leads_scored_df
+            .merge(
+                right=strategy_df[['category']],
+                how='left',
+                left_index=True,
+                right_index=True
+            )
+        )
+
+    if verbose:
+        print("Strategy Created.")
+
+    # return
+    return strategy_df
+
+# ------------------------------------------------------------------------------------------------ #
+
 
 # Workflow
+lead_make_strategy(
+	data   = leads_scored_df,
+	thresh = 0.90,
+	for_marketing_team = False
+)
 
 
 
-# 2.0 AGGREGATE THE LEAD STRATEGY RESULTS
+# ------------------------------------------------------------------------------------------------ #
+#                              2.0 AGGREGATE THE LEAD STRATEGY RESULTS                             #
+# ------------------------------------------------------------------------------------------------ #
+
 #  lead_aggregate_strategy_results()
 
+def lead_aggregate_strategy_results(data):
+
+    # aggregate results
+    results_df = (
+		data
+			.groupby('category')
+			.agg(
+				count = ('made_purchase', 'count'),
+				sum_made_purchase = ('made_purchase', 'sum')
+			)
+	)
+
+    # return
+    return results_df
+
+# ------------------------------------------------------------------------------------------------ #
 
 # Workflow
+(
+	lead_make_strategy(leads_scored_df, thresh = 0.90)
+		.pipe(lead_aggregate_strategy_results)
+)
 
 
-    
-# 3.0 CALCULATE EXPECTED VALUE (FOR ONE SET OF INPUTS) ----
-#  lead_strategy_calc_expected_value()
+# ------------------------------------------------------------------------------------------------ #
+#                       3.0 CALCULATE EXPECTED VALUE (FOR ONE GROUP OF INPUTS)                       #
+# ------------------------------------------------------------------------------------------------ #
+
+# lead_strategy_calc_expected_value()
+
+def lead_strategy_calc_expected_value(
+	data,
+	email_list_size = 100000,
+	unsub_rate_per_sales_email = 0.001,
+	sales_emails_per_month = 5,
+	avg_sales_per_month = 250000,
+	avg_sales_emails_per_month = 5,
+	customer_conversion_rate = 0.05,
+	avg_customer_value = 2000,
+	verbose = False
+):
+    # Define Variables ----
+	cold_lead_count  = data['count'].get('Cold-Lead', 0)
+	hot_lead_count   = data['count'].get('Hot-Lead', 0)
+	missed_purchases = data['sum_made_purchase'].get('Cold-Lead', 0)
+	made_purchases   = data['sum_made_purchase'].get('Hot-Lead', 0)
+
+
+	# Confusion Matrix Summaries ----
+	total_count          = (cold_lead_count + hot_lead_count)
+	total_purchases      = (missed_purchases + made_purchases)
+	sample_factor        = email_list_size / total_count
+	sales_per_email_sent = avg_sales_per_month / avg_sales_emails_per_month
+
+	# ---- PRELIMINARY EXPECTED VALUE CALCULATIONS ---- #
+	# [Savings] Cold That Are Not Targeted ----
+	savings_cold_no_target = (
+		cold_lead_count
+			* (sales_emails_per_month * unsub_rate_per_sales_email)
+			* (customer_conversion_rate * avg_customer_value)
+			* sample_factor
+
+	)
+
+ 	# [Cost] Missed Sales That Are Not Targeted ----
+	missed_purchase_ratio = missed_purchases / (missed_purchases + made_purchases)
+	cost_missed_purchases = (sales_per_email_sent * sales_emails_per_month * missed_purchase_ratio)
+
+	# [Cost] Hot Leads Targeted That Unsubscribe ----
+	cost_hot_target_but_unsub = (
+		hot_lead_count
+			* sales_emails_per_month * unsub_rate_per_sales_email
+			* customer_conversion_rate * avg_customer_value
+			* sample_factor
+	)
+
+	# [Savings] Sales Achieved ----
+	made_purchase_ratio = made_purchases / (missed_purchases + made_purchases)
+	savings_made_purchases = (sales_per_email_sent * sales_emails_per_month * made_purchase_ratio)
+
+
+	# ---- FINAL EXPECTED VALUE TO REPORT TO MANAGEMENT ---- #
+
+	# Expected Monthly Sales (Realized) ----
+	savings_made_purchases
+
+	# Expected Monthly Value (Unrealized because of delayed nuture effect)
+	ev = savings_made_purchases + savings_cold_no_target - cost_missed_purchases
+
+	# Expected Monthly Savings (Unrealized until nurture takes effect)
+	es = savings_cold_no_target - cost_missed_purchases
+
+	# Expected Saved Customers (Unrealized until nuture takes effect)
+	esc = savings_cold_no_target / avg_customer_value
+
+	# ---- EXPECTED VALUE SUMMARY OUTPUT ---- #
+	if verbose:
+		print(f"Expected Value: {'${:,.0f}'.format(ev)}")
+		print(f"Expected Savings: {'${:,.0f}'.format(es)}")
+		print(f"Monthly Sales: {'${:,.0f}'.format(savings_made_purchases)}")
+		print(f"Saved Customers: {'${:,.0f}'.format(esc)}")
+
+	# Return ----
+	return {
+		'expected_value': ev,
+		'expected_savings': es,
+		'monthly_sales': savings_made_purchases,
+		'expected_customers_saved': esc
+	}
 
 
 
 # Workflow:
+lead_make_strategy(
+	data = leads_scored_df,
+	thresh = 0.99,
+	verbose = True
+) \
+    .pipe(
+        lead_aggregate_strategy_results) \
+    .pipe(
+        lead_strategy_calc_expected_value,
+        email_list_size = 200000,
+		unsub_rate_per_sales_email = 0.001,
+		sales_emails_per_month = 5,
+		avg_sales_per_month = 250000,
+		avg_sales_emails_per_month = 5,
+		customer_conversion_rate = 0.05,
+		avg_customer_value = 2000,
+		verbose = False
+    )
 
 
 
-# 4.0 OPTIMIZE THE THRESHOLD AND GENERATE A TABLE
+# ------------------------------------------------------------------------------------------------ #
+#                          4.0 OPTIMIZE THE THRESHOLD AND GENERATE A TABLE                         #
+# ------------------------------------------------------------------------------------------------ #
 #  lead_strategy_create_thresh_table()
 
 
@@ -94,7 +276,15 @@ leads_scored_df = els.model_score_leads(leads_df)
 
 # Business Leaders may freak out if they see a big hit in sales
 #  even though it could be beneficial in long term
-# Recommend to start with a smaller strategy of focusing on 
-#  top 95% 
+# Recommend to start with a smaller strategy of focusing on
+#  top 95%
 # We can set this up with the Threshold Override
+
+# ---------------------------------------------------------------------------- #
+#                            Shift + Opt + X = Title                           #
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------- Opt + X = Subtitle ---------------------------- #
+
+# ---------------------------------------------------------------------------- #
 
--- email_lead_scoring/modeling.py
+++ email_lead_scoring/modeling.py
@@ -1,10 +1,10 @@
 # =========================================================================
-# LEAD SCORING MODULE 
+# LEAD SCORING MODULE
 # =========================================================================
 
 
 # =========================================================================
-# LIBRARIES 
+# LIBRARIES
 # =========================================================================
 import pandas as pd
 import numpy as np
@@ -14,7 +14,7 @@ import mlflow
 # =========================================================================
 # MODEL LOAD FUNCTION
 # =========================================================================
-def model_score_leads(data, model_path = "models/blended_model_final"):
+def model_score_leads(data, model_path = "/models/pycaret/xgb_model_single_tuned_finalized"):
     """_summary_
 
     Args:
@@ -24,20 +24,20 @@ def model_score_leads(data, model_path = "models/blended_model_final"):
     Returns:
         DataFrame: DataFrame of leads along with prediction and score.
     """
-    
+
     # - Load model
     model = clf.load_model(model_path)
-    
+
     # - Get predictions
     predictions_df = clf.predict_model(estimator = model, data = data)
 
     df = predictions_df
-    
+
     predictions_df["Score"] = np.where(df["Label"] == 0, 1 - df["Score"], df["Score"])
-        
+
     # - Score leads
-    leads_scored_df = pd.concat([predictions_df["Score"], data], axis = 1)    
-    
+    leads_scored_df = pd.concat([predictions_df["Score"], data], axis = 1)
+
     # - Return
     return leads_scored_df
 
@@ -46,9 +46,9 @@ def model_score_leads(data, model_path = "models/blended_model_final"):
 # MLFLOW GET BEST RUN
 # =========================================================================
 def mlflow_get_best_run(
-    experiment_name, 
-    n          = 1, 
-    metric     = "metrics.auc", 
+    experiment_name,
+    n          = 1,
+    metric     = "metrics.auc",
     tag_source = ["finalize_model", "h2o_automl_model"],
     ascending  = False
 ):
@@ -59,26 +59,26 @@ def mlflow_get_best_run(
         n (int, optional): _description_. Defaults to 1.
         metric (str, optional): Metric to use. Defaults to "metrics.auc".
         tag_source (list, optional): Tag.Source in MLflow to use in production. Defaults to ["finalize_model", "h2o_automl_model"].
-        ascending (bool, optional): Whether or not to sor the metric ascending or descending. AUC should be descending. Metrics like log loss should be ascending.  
-    
+        ascending (bool, optional): Whether or not to sor the metric ascending or descending. AUC should be descending. Metrics like log loss should be ascending.
+
     Returns:
-        string: The best run id found. 
-    
+        string: The best run id found.
+
     """
-    
-    
-    experiment = mlflow.get_experiment_by_name(experiment_name)    
+
+
+    experiment = mlflow.get_experiment_by_name(experiment_name)
     experiment_id = experiment.experiment_id
-    
+
     logs_df = mlflow.search_runs(experiment_id)\
     .rename(columns = lambda x: x.lower())
-    
+
     best_run_id = logs_df\
         .query(f'`tags.source` in {tag_source}')\
         .sort_values(metric, ascending = ascending)\
         ["run_id"]\
         .values[n - 1]
-    
+
     return(best_run_id)
 
 
@@ -86,9 +86,9 @@ def mlflow_get_best_run(
 # MLFLOW SCORE LEADS
 # =========================================================================
 def mlflow_score_leads(data, run_id):
-    
-    """This function scores the leads using an MLflow Run Id to select a model. 
-    
+
+    """This function scores the leads using an MLflow Run Id to select a model.
+
     Args:
         data (DataFrame): Leads data from els.db_read_and_process_data()
         run_id (string): An MLflow Run Id. Recommend to use mlflow_get_best_run().
@@ -96,22 +96,22 @@ def mlflow_score_leads(data, run_id):
     Returns:
         DataFrame: A dataframe with the lead scores column, concatenated to the leads_df.
     """
-    
+
     logged_model = f"runs:/{run_id}/model"
     print(logged_model)
 
     loaded_model = mlflow.pyfunc.load_model(logged_model)
-    
+
     # Predict
     try:
         predictions_array = loaded_model.predict(pd.DataFrame(data))["p1"]
     except:
         predictions_array = loaded_model._model_impl.predict_proba(data)[:, 1]
-        
+
     predictions_series = pd.Series(predictions_array, name = "Score")
-    
+
     ret = pd.concat([predictions_series, data], axis = 1)
-    
-    
+
+
     # Return
     return ret
